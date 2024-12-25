394ffc7572e8b39bd3432c32b564b67cd2267087
Abdulatif Tarek <138982485+Tifa200@users.noreply.github.com>
2024-12-25 18:43:35 +0200

    Add project files.

---
+++ Demo/Assignment.cs
@@ -0,0 +1,122 @@
+﻿namespace Demo
+{
+    internal class Assignment
+    {
+        //public static void Swap(int x, int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+        //} 
+        //public static void Swap(ref int x,ref int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+        //public static void Calculate(int a, int b, int c, int d)
+        //{ 
+        //    int sum = a + b; int subtraction = c - d; 
+        //    Console.WriteLine("Sum of first two numbers: "+ sum);
+        //    Console.WriteLine("Subtraction of last two numbers: "+ subtraction);
+        //}
+        //public static bool IsPrime(int num) 
+        //{ 
+        //    if (num <= 1) return false; 
+        //    for (int i = 2; i * i <= num; i++)
+        //    { 
+        //        if (num % i == 0) 
+        //            return false;
+        //    } 
+        //    return true; 
+        //}
+        //public static void MinMaxArray(int[] array, ref int min, ref int max)
+        //{
+        //    foreach (int num in array)
+        //    {
+        //        if (num < min) min = num;
+        //        if (num > max) max = num;
+        //    }
+        //}
+        //public static int CalculateFactorial(int n) 
+        //{
+        //    int result = 1; 
+        //    for (int i = 1; i <= n; i++) 
+        //    { 
+        //        result *= i; 
+        //    }
+        //    return result; 
+        //}
+        static void Main(string[] args)
+        {
+            #region ِAssignment 5
+            #region Q1
+            //This is useful when you want to ensure that the original data remains unchanged after the method execution.
+            //int a = 5;
+            //int b = 9;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Swap(a, b);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b); 
+            #endregion
+            #region Q2
+            //Useful when you want to ensure the method can't change the reference to point to a new object, but can still modify the object's internal state.
+            //int a = 5;
+            //int b = 9;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Swap(ref a,ref  b);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            #endregion
+            #region Q3
+            //Console.Write("First number: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.Write("Second number: ");
+            //int num2 = int.Parse(Console.ReadLine()); 
+            //Console.Write("Third number: ");
+            //int num3 = int.Parse(Console.ReadLine()); 
+            //Console.Write("Fourth number: ");
+            //int num4 = int.Parse(Console.ReadLine());
+            //Calculate(num1, num2, num3, num4);
+            #endregion
+            #region Q4
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //int temp = number;
+            //while (temp != 0)
+            //{
+            //    sum += temp % 10;
+            //    temp /= 10;
+            //}
+            //Console.WriteLine($"The sum of the digits of the number {number} is: {sum}");
+            #endregion
+            #region Q5
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine()); 
+            //bool isPrime = IsPrime(number);
+            //Console.WriteLine("Is the number prime -> "+isPrime);
+            #endregion
+            #region Q6
+            //int[] numbers = { 1,2,3,4,5,6,7,8,9 };
+            //int min = int.MaxValue; 
+            //int max = int.MinValue;
+            //MinMaxArray(numbers, ref min, ref max);
+            //Console.WriteLine($"The maximum value in the array is: " + max);
+            //Console.WriteLine($"The minimum value in the array is: " +min); 
+            #endregion
+            #region Q7
+            //Console.Write("Enter a number  ");
+            //int number = int.Parse(Console.ReadLine());
+            //int factorial = CalculateFactorial(number);
+            //Console.WriteLine("The factorial  is: "+factorial);
+            #endregion
+            #region Q8
+
+            #endregion
+            #endregion
+        }
+    }
+}
---
+++ Demo/Demo.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+
+//public static void PrintShape(int count, string pattern)
+//{
+//    for (int i = 0; i < count; i++) {
+//        Console.WriteLine(pattern);
+//    } 
+{
+    internal class Demo
+    {
+        #region Demo
+        #region Casting
+        //object obj = new object();
+        //obj = "mostafa";
+        //obj = 3;
+        //int x = 10;
+        //object obj01 = new object();
+        //obj01 = x;
+        //Console.WriteLine(x);
+        //object obj02 = new object();
+        //obj02 = 10;
+        //int x = (int)obj02;
+        #endregion
+        #region Nullable DataType
+        //int x = null;
+        //int? x = null;
+        //Nullable<int> number;
+        //int?  x =10;
+        //int y =(int) x;
+        //int y;
+        //if (x != null)
+        //{
+        //    y = (int)x;
+        //}
+        //else {
+        //    y = 0;
+        //}
+        //int y;
+        //if (x.HasValue)
+        //{
+        //    y =x.Value;
+        //}
+        //else {
+        //    y = 0;
+        //}
+        //y = HasValue ? x.Value : 0;
+        //Console.WriteLine("y");
+        #endregion
+        #region Refrence
+        //string? name = null;
+        //Console.WriteLine(name);
+        #endregion
+        #region Null Propagation Operator
+        //int[] numbers = { 1, 2, 3, 4, 5 };
+        //if (numbers != null)
+        //{
+        //    for (int i = 0; numbers != null && i < numbers.Length; i++)
+        //    {
+        //        Console.WriteLine(numbers[i]);
+        //    }
+        //}
+        //int length = numbers?.length ? 0 ;
+        //Console.WriteLine(length);
+        //Employee EMP = new Employee();
+
+        //if (Employee!=null)
+        //{
+        //    if (EMP.DEpartment != null)
+        //    {
+        //        Console.WriteLine(EMP.Department.Name);
+        //    }
+        //}
+        //Console.WriteLine(EMP?.Department?.Name??"Not Found");
+        #endregion
+        #region Functions
+        //PrintShape(10, "*_*");
+        //Console.WriteLine("Hello \n Mostafa Welcome To\t Route ");
+        //PrintShape(10, "/*\\");
+        #endregion
+        #region Value type Parameters
+        #region by value
+        //int a = 5;
+        //int b = 9;
+        //Console.WriteLine(a);
+        //Console.WriteLine(b);
+        //Swap(a, b);
+        //Console.WriteLine(a);
+        //Console.WriteLine(b); 
+        #endregion
+        #region by ref
+        //int a = 5;
+        //int b = 9;
+        //Console.WriteLine(a);
+        //Console.WriteLine(b);
+        //Swap(ref a,ref  b);
+        //Console.WriteLine(a);
+        //Console.WriteLine(b);
+        #endregion
+        #region Reference Type
+        #region By Value
+        //int[] Numbers = { 1, 2, 3 };
+        //Console.WriteLine(SumArray(Numbers));
+        //Console.WriteLine(Numbers[0]);
+
+        #endregion
+        #endregion
+        #region Ex02
+        #region By Value 
+        //int[] Numbers = { 1, 2, 3 };
+        //Console.WriteLine(SumArray(Numbers));
+        //Console.WriteLine(Numbers[0]);
+
+        #endregion
+
+        #region By Reference
+
+        //int[] Numbers = { 1, 2, 3 };
+        //Console.WriteLine(ref Numbers);
+        //Console.WriteLine(Numbers[0]);
+
+        #endregion
+
+        #endregion
+        #region Functions - Passing By Out
+        //int A = 10;
+        //int B = 20;
+        ////int Sum, Mul;
+        //int Sum = 0 , Mul = 0 ; 
+        ////SumMul(A, B, out Sum,out Mul);
+        //SumMul(A, B, ref Sum,ref Mul);
+
+        //Console.WriteLine(Sum);
+        //Console.WriteLine(Mul);
+        #endregion
+        #region Functions - Parms
+
+        //int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //Console.WriteLine(SumArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        //int A = 10, B = 20, C = 30;
+        //Console.WriteLine("A : {0} , B : {1} , C : {2}", A, B, C);
+
+        #endregion
+        #endregion
+    }
+    #endregion
+    
+}
